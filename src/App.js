757daa384dbc24d71108344030ef0acf14043cfb
Brennan Jarchow <spacetalon@gmail.com>
2022-12-12 18:43:31 -0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HeadFoot from './HeadFoot';
+import MyAccount from './MyAccount';
+
+function App() {
+  return (
+    <div>
+      <MyAccount />
+    </div>
+  );
+}
+
+export default App;
